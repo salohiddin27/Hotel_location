dab19bab598954f6ac778f0ffd60c468d470ca91
salohiddin27 <salohiddinnurbayev25@gmail.com>
2026-01-30 03:33:24 +0500

    Admin yaratish fayli qo'shildi

---
+++ createsuperuser.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings') # 'root' o'rniga loyiha papkangiz nomi bo'lishi mumkin
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = 'admin'
+email = 'admin@example.com'
+password = '1' # O'zingizga qulay parol qo'ying
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superuser '{username}' muvaffaqiyatli yaratildi!")
+else:
+    print(f"Superuser '{username}' allaqachon mavjud.")
